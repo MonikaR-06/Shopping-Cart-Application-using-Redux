463c586c2f7a7e1df69dc7e7eaed118b9416a288
monikaa06 <writetomonika06@gmail.com>
2024-08-18 19:58:32 +0530

    first commit

---
+++ src/products.js
@@ -0,0 +1,74 @@
+import imageProduct1 from './assets/images/1.png' 
+import imageProduct2 from './assets/images/2.png' 
+import imageProduct3 from './assets/images/3.png' 
+import imageProduct4 from './assets/images/4.png' 
+import imageProduct5 from './assets/images/5.png' 
+import imageProduct6 from './assets/images/6.png' 
+import imageProduct7 from './assets/images/7.png' 
+import imageProduct8 from './assets/images/8.png' 
+export const products = [
+    {
+        id: 1,
+        name: 'Deskart Tissue Paper Holder',
+        price: 305,
+        image: imageProduct1,
+        description: 'Durable wooden MDF (Medium Density Fiberboard) construction, Sleek and modern design, Multiple color and finish options to match any décor, Compact and space-saving design, Easy to clean and maintain, Ideal for use in kitchen, office, bedroom, resturant and cafeteria, Helps keep tissues organized and easily accessible, Adds a touch of warmth and elegance to the space, Sturdy and long-lasting construction for years of use, Suitable for both residential and commercial environments',
+        slug: 'hertfoid-upholstered-chair'
+    },
+    {
+        id: 2,
+        name: 'Cri8Hub Stainless Steel Kitchen Rack',
+        price: 299,
+        image: imageProduct2,
+        description: 'FUNCTIONAL & VERSATILE: The perfect solution for organizing a multitude of items - such as video games, bath soaps, shampoos, conditioners, linens, towels, laundry needs, craft supplies and more; Use in bathroom, bedroom, closet, laundry room, utility room, garage, hobby and craft room, home office, mudroom and entryways; Perfect for homes, apartments, condos, college dorm rooms, RVs and campers SMART STORAGE: Use anywhere you need to keep food, pantry and kitchen items organized; Perfect for organizing pasta, packets, extra supplies of salt, dressing and vinegar, macaroni and cheese boxes, baking supplies, condiments and more; These bins also work in any room of your home!, MATERIAL: Constructed of durable steel with a gleaming chrome finish, the stylish two-shelf shelving unit offers the best of both rugged reliability and modern appeal!, MULTIPURPOSE : trolley can be used for multi purposes, like storing fruits and vegetables,keeping books,or any other cosmetic items or cookware items and house products!, 2 SHELVES : This trolley has 2 shelves, which makes it large enough to store many things at a time!',
+        slug: 'bingdon-pholstered-chair-swivel'
+    },
+    {
+        id: 3,
+        name: 'Soap Dispenser for Kitchen',
+        price: 199,
+        image: imageProduct3,
+        description: '2 IN 1 SOAP DISPENSER HIGH-QUALITY MATERIAL: The kitchen soap sponge holder is made of premium ABS + PC transparent material, safe and BPA-free, environmentally friendly and healthy, corrosion-resistant. It comes into direct contact with liquids without undergoing any chemical reactions which is durable enough for you to use for long-time reuse., LARGE CAPACITY & EASY DISPENSING: Sponge soap dispenser only needs to gently press down with one hand to dispense an appropriate amount of soap onto the sponge, extremely easy to use and helps reduce soap wasted. The dish soap dispenser has a capacity of 400ML so that you don’t need to refill it frequently, And suitable for all kinds of liquid soap.',
+        slug: 'jeses-minimore-modern-style-etta'
+    },
+    {
+        id: 4,
+        name: 'Freezer Storage Containers',
+        price: 279,
+        image: imageProduct4,
+        description: 'Durable Plastic Containers for Kitchen Storage: These freezer storage containers, made of high-quality plastic, are perfect for kitchen storage. They are durable, easy to clean, and perfect for storing a variety of items, Kitchen Storage Containers: Our storage containers for kitchen, keeping your food fresh and safe. They are perfect for storing dry goods like masala, making them an essential kitchen accessory item.',
+        slug: 'eses-minimore-modern-style-zakari'
+    },
+    {
+        id: 5,
+        name: 'Items organizer Rack Stand',
+        price: 294,
+        image: imageProduct5,
+        description: 'NO DRILLING REQUIRED, EASY TO INSTALL, NOTE-MAKE SURE YOU ARE USING THE PRODUCT ON CLEAN AND SMOOTH SURFACE, PACKAGE CONTENT- 2PC OF STICKERS, 9 HOOKS AND STAINLESS STEEL RAIL',
+        slug: 'corrigan-studio-tufed-upholstered-wide-winback-armchair'
+    },
+    {
+        id: 6,
+        name: 'Stainless Steel Holder with Hook',
+        price: 299,
+        image: imageProduct6,
+        description: '【Get your home and life in order】: Tired of opening your closet to find a mountain of tools falling in every imaginable direction? Our broom holder could solve this problem and make sure everything stays in place,【Strong and long service life】: Made of high quality SUS 304 stainless steel, waterproof and rustproof. Triangle buckle and petal-shaped gear automatically adjust the size to the different thickness of the handles to prevent the object from slipping.',
+        slug: 'corrigan-studio-273-wide-armchair'
+    },
+    {
+        id: 7,
+        name: 'Ice Cube Tray Silicone',
+        price: 299,
+        image: imageProduct7,
+        description: 'FOOD SAFE MATERIAL - This ice cube tray Made of food grade silicone, which ensures the drink,food safety for you. MUST HAVE KITCHEN GADGET - Just Fill the Ice Cube Mould with Water, Fruits,Ice cream, Wine and What Something you like. EASY EXTRACTION - Non-stick silicone ice trays makes the ice cube remove out easily, a simply push from behind with your fingers, ice cubes will slide out like butter. Dishwasher safe. SLOW MELTING ICE CUBES – The freezed ice melts slowly, great for keeping a drink cold all night. MULTI PURPOSE - We can use this tray for making Ice candy, Ice cube',
+        slug: 'leston-wide-upholstered-fabric'
+    },
+    {
+        id: 8,
+        name: 'Silicon Spatulas for Cooking',
+        price: 425,
+        image: imageProduct8,
+        description: 'NO MORE CHEAP, UNHYGIENIC SILICONE SPATULA FOR KITCHEN-These premium solid Wooden spatula for non stick pan with tools set & wooden Handle wont harbor bacteria or let you down by melting or warping. Spatula Set with smooth edges wont scratch non-stick pans, and are perfect for spreading frosting or scraping out mixing bowls. SAFE AND HEALTHY WOODEN KITCHEN UTENSILS FOR YOUR FAMILY - We use these silicone spatula set ourselves and want the best for our family and yours. Our silicone is safety-tested, heat resistant up to 480F, and guaranteed 100% BPA-free',
+        slug: 'stephanny-275-wide-tufted-armchair'
+    },
+];
